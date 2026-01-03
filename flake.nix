9db8db6ff5f188d228530f21e93e808dd29f0d2b
jetcookies <226018678+jetcookies@users.noreply.github.com>
2026-01-03 23:35:39 +0800

    feat(flake.nix): add nixosModules to flake outputs

--- flake.nix
+++ flake.nix
@@ -10,5 +10,9 @@
         pkgs = import nixpkgs { inherit system; };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+
+      nixosModules = {
+        subconverter = import ./modules/services/networking/subconverter.nix;
+      };
     };
 }
