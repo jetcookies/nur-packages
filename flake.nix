a4c7fbedda91a1d951ca64b5c514389ef8f9c947
jetcookies <226018678+jetcookies@users.noreply.github.com>
2026-01-22 20:58:26 +0800

    refactor(flake.nix): refactor outputs using flake-parts
    https://flake.parts

--- flake.nix
+++ flake.nix
@@ -1,18 +1,22 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
 
-      nixosModules = {
-        subconverter = import ./modules/services/networking/subconverter.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+        legacyPackages = import ./default.nix { inherit pkgs; };
+        packages = nixpkgs.lib.filterAttrs (_: nixpkgs.lib.isDerivation) legacyPackages;
+      };
+      flake = {
+        nixosModules = {
+          subconverter = import ./modules/services/networking/subconverter.nix;
+        };
       };
     };
 }
