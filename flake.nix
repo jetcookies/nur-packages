45facdcf81ac745041acc6702ffe12c507641175
jetcookies <226018678+jetcookies@users.noreply.github.com>
2026-01-23 21:53:43 +0800

    refactor(flake.nix): add lib as input attr of mkFlake

--- flake.nix
+++ flake.nix
@@ -6,19 +6,22 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
-        legacyPackages = import ./default.nix { inherit pkgs; };
-        packages = nixpkgs.lib.filterAttrs (_: nixpkgs.lib.isDerivation) legacyPackages;
-      };
-      flake = {
-        nixosModules = {
-          subconverter = import ./modules/services/networking/subconverter.nix;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+        perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+          legacyPackages = import ./default.nix { inherit pkgs; };
+          packages = lib.filterAttrs (_: lib.isDerivation) legacyPackages;
         };
-      };
-    };
+        flake = {
+          nixosModules = {
+            subconverter = import ./modules/services/networking/subconverter.nix;
+          };
+        };
+      }
+    );
 
   nixConfig = {
     extra-substituters = [
